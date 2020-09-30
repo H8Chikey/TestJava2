3ee84b480962b0274a3202819c2373d662577ac6
Molokin <molokin_sasha@mail.ru>
2020-09-30 14:27:58 +0300

    haha

--- src/ru/sapteh/Program.java
+++ src/ru/sapteh/Program.java
@@ -1,40 +1,32 @@
 package ru.sapteh;
 
-import javax.imageio.IIOException;
-import java.io.*;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class Program {
-    public static void main(String[] args) throws Exception {
-        File dir = new File("C:/java_42/1/OOP/dir");
-        dir.mkdir();
-        File file = new File("C:/java_42/1/OOP/dir/Project.txt");
-        file.createNewFile();
-        FileWriter fw = new FileWriter(file);
-        var buf = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("Введите имя:");
-        String name = buf.readLine();
-        System.out.println("Введите возраст:");
-        int age = Integer.parseInt(buf.readLine());
-        System.out.println("Введите дату:");
-        int date = Integer.parseInt(buf.readLine());
+    public static void main(String[]arg){
 
-        Person person = new Person(name,age,date);
-        fw.write(personIsEmpty(person));
-        System.out.println(personIsEmpty(person));
-        fw.close();
+
+        Scanner scanner= new Scanner(System.in);
+        System.out.println("Введите имя: ");
+        String name = scanner.nextLine();
+        System.out.println("Введите возраст: ");
+        int  age = scanner.nextInt();
+        System.out.println("Введите дтау рождения: ");
+        int date = scanner.nextInt();
+        Person person = new Person(name,age, date );
 
     }
+
     public static String personIsEmpty(Person p){
         String err = "";
-        if (p.getName().isEmpty()) err += "Имя не заполнено\n";{
-            if (p.getAge() == 0) err += "Возвраст не заполнен\n";{
-                if (p.getDate() == 0) err +="Дата не заполнена"; {
-            if (!err.isEmpty()) return err;
+        if(p.getName().isEmpty()) err += "NoName";
+        if(p.getAge() ==0) err += "NoAge";
+        if(p.getDate() ==0) err += "NoDate";
+        if(err.isEmpty()) return err;
+        return "объект" + p.getClass() + "инициализирован\n" + p.toString();
 
-            return  "Объект" + p.getClass() +
-                    "Иницилизирован\n" + p.toString();
-        }
-    }
-        }
     }
 }
